ad06b7bf4f5d6bf3e314d56aafa215721182a639
kkhabznya <k.khabaznya@yandex.ru>
2025-12-10 17:38:14 +0300

    tada

---
+++ main.py
@@ -0,0 +1,128 @@
+from lark import Lark, Transformer, UnexpectedToken, UnexpectedCharacters, UnexpectedEOF
+
+grammar = '''
+start: _NL? (variable|COMMENT)*
+
+variable: "global" name "=" value _NL?
+value   : operation
+        | string
+        | array
+        | number
+    
+name    : /[_a-zA-Z][_a-zA-Z0-9]*/
+number  : /[+-]?\d+\.?\d*[eE][+-]?\d+/
+array   : "[" [value ("," value)*] "]"
+string  : /[\'].[^']*[\']/
+COMMENT : "*" /.+/ _NL?
+
+operation   : "@{" oper_type "}"
+oper_type   : "+" name number   -> oper_add
+            | "-" name number   -> oper_sub
+            | "*" name number   -> oper_mul
+            | "/" name number   -> oper_div
+            | "min" name number -> oper_min
+            
+%import common.NEWLINE -> _NL
+%import common.WS_INLINE
+%ignore WS_INLINE
+%ignore COMMENT
+'''
+#"{:e}".format(test)
+
+class TreeToJson(Transformer):
+    def start(self, token):
+        return token
+    def variable(self, token):
+        return token
+    def value(self, token):
+        return token[0]
+    def name(self, token):
+        return {"name":token[0].value}
+    def number(self, token):
+        return {"number":token[0].value}
+    def string(self, token):
+        return {"string":token[0].value}
+    def array(self, token):
+        return {"array":token}
+    def operation(self, token):
+        return token[0]
+    def oper_add(self, token):
+        return {"operation":token,"type":"+"}
+    def oper_sub(self, token):
+        return {"operation":token,"type":"-"}
+    def oper_mul(self, token):
+        return {"operation":token,"type":"*"}
+    def oper_div(self, token):
+        return {"operation":token,"type":"/"}
+    def oper_min(self, token):
+        return {"operation":token,"type":"min"}
+    
+def ignore_errors(e):
+    print(e)
+    return True
+
+grammar_parser = Lark(grammar,parser='lalr',transformer=TreeToJson())
+
+variable_list = []
+variable = {}
+
+def tryParse(user_input):
+    try:
+        parsed = grammar_parser.parse(user_input)
+    except UnexpectedToken as e:
+        print(f"Unexpected token at line {e.line}, column {e.column}. Expected one of: {e.expected}")
+        return 0
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        return 0
+    else:
+        if user_input != '' and parsed != []:
+            parse(parsed[0])
+
+def parse(parsing):
+    for item in parsing:
+        if item.get("operation"):
+            result = processFunction(item)
+            temp_parse = parsing[0]
+            parsing = []
+            parsing.append(temp_parse)
+            parsing.append(result)
+    variable_list.append(parsing)
+    
+def recurse(where,what,exact):
+    for item in where:
+        if item.get(what)==exact:
+            print(item)
+            print(exact)
+            return True
+    return False
+
+def processFunction(item):
+    number_1 = item.get("operation")[0].get("name")
+    for thing in variable_list:
+        if thing[0].get("name")==number_1:
+            print(thing[1])
+            if not(thing[1].get("number")):
+                print("error")
+                return 0
+            number_1 = float(thing[1].get("number"))
+    number_2 = float(item.get("operation")[1].get("number"))
+    print(number_1," ",number_2)
+    match item.get("type"):
+        case '+':
+            return {"number":number_1+number_2}
+        case '-':
+            return {"number":number_1-number_2}
+        case '/':
+            return {"number":number_1/number_2}
+        case '*':
+            return {"number":number_1*number_2}
+        case 'min':
+            return min(number_1,number_2)
+    return {'number':f'result'}
+
+user_input = -1;
+while user_input != '':
+    user_input = str(input())
+    tryParse(user_input)
+    print(f"Current variables: {variable_list}")
